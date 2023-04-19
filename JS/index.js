420cccf5f68ceeedf42d5d3f30200fd155bb0a6a
Alexey <alexeykarpenko2015@gmail.com>
2023-04-19 10:24:50 +0100

    some changes

--- JS/index.js
+++ JS/index.js
@@ -3,46 +3,40 @@ const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
 const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 
 
-
-// // ФУНКЦІЯ 1
-// // Створіть функцію getMyTaxes.call(country, salary) -> number; –
-// // яка рахує скільки податків ви заплатите як IT-спеціаліст в якійсь з країн.
-// // Функція повинна викликатись через call та працювати з даними через this.
-
+            // ФУНКЦІЯ 1
+// Рахує скільки податків ви заплатите як IT-спеціаліст в якійсь з країн.
+// Функція повинна викликатись через call та працювати з даними через this.
 function getMyTaxes(mySalary,country) {
-    return (mySalary * this.tax).toFixed(2);
+  return (mySalary * this.tax).toFixed(2);
 }
+
 console.log(getMyTaxes.call(ukraine,100000));
 
-// // ФУНКЦІЯ 2
-// // Створіть функцію getMiddleTaxes.call(country) -> number; –
-// // яка рахує скільки у середньому податків платятт IT-спеціалісти у кожній країні. (tax * middleSalary).
-// // Функція повинна викликатись через call та працювати з даними через this.
 
-function getMiddleTaxes(country){
-    return (this.middleSalary * this.tax).toFixed(2);
+            // ФУНКЦІЯ 2
+//Рахує скільки у середньому податків платятт IT-спеціалісти у кожній країні. 
+// Функція повинна викликатись через call та працювати з даними через this.
+function getMiddleTaxes(country) {
+  return (this.middleSalary * this.tax).toFixed(2);
 }
+
 console.log(getMiddleTaxes.call(ukraine)); 
 
 
-// // ФУНКЦІЯ 3
-// // Створіть функцію getTotalTaxes.call(country) -> number; –
-// // яка рахує, скільки всього податків платять IT-спеціалісти у кожній країні. (tax middleSalary vacancies).
-// // Функція повинна викликатись через call та працювати з даними через this.
-
+          // ФУНКЦІЯ 3
+//Рахує,скільки всього податків платять IT-спеціалісти у кожній країні. 
+// Функція повинна викликатись через call та працювати з даними через this.
 function getTotalTaxes(country){
     return (this.middleSalary * this.tax * this.vacancies).toFixed(2);
 }
+
 console.log(getTotalTaxes.call(ukraine)); 
 
 
-// ФУНКЦІЯ 4
-// Створіть функцію getMySalary(country) – яка буде писати в консоль об'єкт виду:
-// { salary: number, taxes: number, profit: number } кожні 10 секунд.
+          // ФУНКЦІЯ 4
+//Буде писати в консоль об'єкт виду:{ salary: number, taxes: number, profit: number } кожні 10 секунд.
 // Значення salary – генеруйте випадковим чином у діапазоні 1500-2000.
 // taxes – розраховується в залежності від вибраної країни та значення salary.
-// profit = salary - taxes;
-
 function getMySalary(country) {
 
     function getRandomSalary(min, max) {
@@ -61,7 +55,8 @@ function getMySalary(country) {
                         }    
                         let profit = getProfit(salary,taxes).toFixed(2);
                         
-                            let result = {salary:salary,taxes:taxes,profit:profit}     
+                            let result = {salary:salary,taxes:taxes,profit:profit}  
+                             
     console.log(result);       
 }              
 setInterval(() => getMySalary(ukraine),10000)
